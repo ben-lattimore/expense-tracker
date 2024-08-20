71c991c360085a933b0c317f21ea0d57127988c3
Ben Lattimore <hello@benlattimore.com>
2024-08-20 12:47:18 +0100

    Changes to link Heroku hosted backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Expense, Category } from '../store/expenseStore';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5002/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = {
     getExpenses: async (): Promise<Expense[]> => {
